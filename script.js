2cce94350817fc542650138503dde4800708fdcf
Aniket S. Jadhav <114282777+ANI62@users.noreply.github.com>
2023-08-31 19:26:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+Shery.mouseFollower();
+Shery.makeMagnet("#magnet");
+Shery.makeMagnet(".headings");
+Shery.makeMagnet(".btndiv");
+Shery.makeMagnet(".tag");
+Shery.hoverWithMediaCircle(".hvr", {
+  videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
+});
+
+gsap.to(".fleftelem", {
+  scrollTrigger: {
+    trigger: "#fimages",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: Power1,
+});
+
+let sections = document.querySelectorAll(".fleftelem");
+Shery.imageEffect(".images", {
+  style: 4,
+  config: { onMouse: { value: 1 } },
+  slideStyle: (setScroll) => {
+    sections.forEach(function (section, index) {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        scrub: 1,
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
+});
